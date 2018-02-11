e13398232661c30db00ccf7b50e1e981f1c1c2b3
vinicius-pretto <vini.prettodasilva@gmail.com>
2018-02-11 19:51:38 -0200

    Adding message form

--- client/js/connection.js
+++ client/js/connection.js
@@ -1,10 +1,32 @@
-function showMessage(status) {
+function showConnectionMessage(status) {
   const message = status === 'success' ? 'Successfull' : 'Unsuccessful';
   $('#connection-status #status').innerHTML = message;
 
   const connnectionStatusMesssage = $('#connection-status');
   connnectionStatusMesssage.className += `alert alert-${status}`;
   connnectionStatusMesssage.style.display = 'block';
+  showMessageForm();
+}
+
+function showMessageForm() {
+  function buildMessageForm() {
+    return `
+      <form class="form-message">
+        <div class="form-group">
+          <label for="to">To</label>
+          <input id="to" type="text" class="form-control" placeholder="name@domain (jid)" required>
+        </div>
+  
+        <div class="form-group">
+          <label for="message">Message</label>
+          <textarea name="message" id="message" rows="3" class="form-control" placeholder="Write here your message.."></textarea>
+        </div>
+        <button class="btn btn-primary">Send message</button>
+      </form>
+    `
+  }
+
+  $('.container').innerHTML += buildMessageForm();
 }
 
 function startConnection(evt) {
@@ -18,15 +40,15 @@ function startConnection(evt) {
   connection.connect(jid, password, (status) => {
     if (status === Strophe.Status.DISCONNECTED) {
       console.warn(`Disconnected from Ejabberd, statusCode: ${status}`);
-      showMessage('danger');
+      showConnectionMessage('danger');
     }
     else if (status === Strophe.Status.CONNTIMEOUT) {
       console.error(`Connection timeout with Ejabberd, statusCode: ${status}`);
-      showMessage('danger');
+      showConnectionMessage('danger');
     }
     else if (status === Strophe.Status.CONNECTED) {
       console.log(`Connection stablished with Ejabberd, statusCode: ${status}`)
-      showMessage('success');
+      showConnectionMessage('success');
     }
   });  
 }
