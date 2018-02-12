2635f10b769b72cad4f70d5c44b2cb3e7c1e8b43
vinicius-pretto <vini.prettodasilva@gmail.com>
2018-02-12 16:09:02 -0200

    FIx

--- client/js/message/message.controller.js
+++ client/js/message/message.controller.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class MessageController {
   constructor(messageService) {
     this.messageService = messageService;
@@ -12,27 +11,6 @@ class MessageController {
       type: 'chat'
     }
     this.messageService.sendMessage(message, params);
-=======
-const $ = (elem) => document.querySelector(elem);
-
-class MessageController {
-  constructor(messageService) {
-    this.messageService = messageService;
-    this.send = this.send.bind(this);
-  }
-
-  send() {
-    const messageBody = $('#message').value;
-    const to = $('#to').value;
-    const from = $('#from').value;
-
-    const params = { 
-      to: to, 
-      from: from, 
-      type: 'chat' 
-    }
-    this.messageService.send(messageBody, params);
->>>>>>> a9147ec57b69d907779a339c2dbbadf2641d6f47
   }
 }
 
--- client/js/message/message.service.js
+++ client/js/message/message.service.js
@@ -3,17 +3,10 @@ import { $msg } from 'strophe.js';
 class MessageService {
   constructor(connection) {
     this.connection = connection;
-<<<<<<< HEAD
     this.sendMessage = this.sendMessage.bind(this);
   }
 
   sendMessage(messageBody, params) {
-=======
-    this.send = this.send.bind(this);
-  }
-
-  send(messageBody, params) {
->>>>>>> a9147ec57b69d907779a339c2dbbadf2641d6f47
     const message = $msg(params)
       .c('body')
       .t(messageBody);
